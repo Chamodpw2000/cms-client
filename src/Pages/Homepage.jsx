19cf696fe3492f370b70471f3c4d2d3e41886f32
Chamodpw2000 <chamodpwanigasekara2000@gmail.com>
2024-10-12 08:22:03 +0530

    word system corrected

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
 
         <h1 className='home-title'>
 
-            CONTENT MANAGEMENT STSTEM
+            CONTENT MANAGEMENT SYSTEM
         </h1>
 
         <p className='home-description'>
